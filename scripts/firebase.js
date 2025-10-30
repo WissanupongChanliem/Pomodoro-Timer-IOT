53c58dddda87b051b017ff48c904d17c0a623568
Wissanupong-Chanliem <wissanupongdev@gmail.com>
2025-10-30 23:11:38 +0700

    connect to firebase, build dashboard on home page using chartjs

---
+++ scripts/firebase.js
@@ -0,0 +1,91 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-app.js";
+import { getDatabase, ref, get, child, onValue } from "https://www.gstatic.com/firebasejs/12.4.0/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyC6vYMSD4HvgwI_fHzmT8eV4tFfqTWcNis",
+    authDomain: "pomodoroiot.firebaseapp.com",
+    databaseURL: "https://pomodoroiot-default-rtdb.asia-southeast1.firebasedatabase.app",
+    projectId: "pomodoroiot",
+    storageBucket: "pomodoroiot.firebasestorage.app",
+    messagingSenderId: "11062092109",
+    appId: "1:11062092109:web:f191926448a30e298a1704"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getDatabase(app);
+
+
+const sessionCountRef = ref(db, "/Session");
+const sessionsRef = ref(db, "/");
+let chart = null;
+
+onValue(sessionCountRef, (snapshot) => {
+  const data = snapshot.val();
+  $("#session-count-text").text(data);
+});
+
+onValue(sessionsRef, (snapshot) => {
+    let totalTime = 0;
+    let chartData = [];
+    snapshot.forEach((childSnapshot) => {
+        const childKey = childSnapshot.key;
+        const childData = childSnapshot.val();
+        if (childKey!="Session")
+        {
+            if (childData.round_studied)
+            {
+                totalTime += childData.round_studied * 25;
+                chartData.push([parseInt(childKey.slice(7)), childData.round_studied * 25]);
+            }
+            else{
+                chartData.push([parseInt(childKey.slice(7)), 0]);
+            }
+        }
+    });
+    chartData.sort((a, b) => {
+        return a[0] - b[0];
+    });
+    updateChart(chartData);
+    if (totalTime > 60)
+    {
+        $("#study-time-text").text((totalTime/60).toFixed(2) + " hour");
+    }
+    else{
+        $("#study-time-text").text(totalTime + " minutes");
+    }
+    
+});
+
+async function updateChart(data){
+    let canvas = document.getElementById('usage');
+
+    if(chart){
+        chart.clear();
+        chart.destroy();
+    }
+    chart = new Chart(
+        canvas,
+        {
+        type: 'bar',
+        data: {
+            labels: data.map(row => row[0]),
+            datasets: [
+            {
+                label: 'Time (minutes) spend on each session',
+                data: data.map(row => row[1]),
+                backgroundColor: '#fc535c'
+            }
+            ]
+        },
+        options: {
+            responsive: true,
+            maintainAspectRatio: false
+        }
+        }
+    );
+}
