3799ce60ffc5d0eed59294f0ec4ff80dbc5bca93
CyanKagami <boonsitapoopeko@gmail.com>
2025-10-30 23:16:54 +0700

    Cleaning code

--- pomodoro.ino
+++ pomodoro.ino
@@ -9,12 +9,46 @@ void  print_timer();
 LiquidCrystal_I2C lcd(0x27,  16, 2);
 Firebase fb(REFERENCE_URL, AUTH_TOKEN);
 WiFiClient network;
+
+/* for on in firebase*/
 String   stored_round = String();
 String   stored_session = String();
 int      session;
 
+// for wifi connecting
 int status = 0;
 int respond_code;
+
+// time interval
+const int LONG_BREAK = 3;
+const int SHORT_BREAK = 2;
+const int WORKING = 1;
+
+/* for button state checking */
+int   state;
+int   stop_button_state;
+int   stop_button_old_state;
+int   restart_button_state;
+int   restart_button_old_state;
+
+/* for counting time*/
+unsigned long old_millis;
+unsigned long restart = 0;
+int   minute;
+int   second;
+int   time_interval;
+int   read_round;
+
+// check afk
+int   afk_second;
+
+// for simulate open device
+int   is_open = 1;
+
+/* for saving data in db*/
+int get_round = 0;
+int loss_focus_round = 0;
+
 
 /*-- emoji-- */
 
@@ -85,21 +119,37 @@ byte smile[8] = {
 };
 
 /*--------*/
+
+/* ultra sonic*/
+const int trigPin = 4;
+const int echoPin = 5;
+
+/* button */
+const int stopButton = 3;
+const int restartButton = 2;
+
+/* LED ( set low to open led) */
+const int RED = 11;
+const int GREEN = 10;
+const int BLUE = 9;
+
+/* A4 -> SDA/ A5 -> SC*/
+
 void setup() {
   // put your setup code here, to run once:
   
   // button pin
-  pinMode(2, INPUT);
-  pinMode(3, INPUT);
+  pinMode(restartButton, INPUT);
+  pinMode(stopButton, INPUT);
 
   // lcd pin
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
 
   // ultrasonic
-  pinMode(4, OUTPUT); // trig
-  pinMode(5, INPUT); // echo
+  pinMode(trigPin, OUTPUT); // trig
+  pinMode(echoPin, INPUT); // echo
 
   
   Serial.begin(9600);
@@ -123,7 +173,7 @@ void setup() {
     }
     Serial.println("WiFi Connected");
   
-  
+  // setting new session and old session status
   fb.getInt("Session", session);
   stored_round = String("session") + session;
   stored_session = stored_round + "/status";
@@ -156,7 +206,7 @@ void setup() {
             Serial.println(respond_code);
   }
 
-  /* emoji */
+  /* emoji set up*/
   lcd.createChar(1, pencil);
   lcd.createChar(2, cross);
   lcd.createChar(3, coffee);
@@ -165,38 +215,17 @@ void setup() {
    init();
 }
 
-const int LONG_BREAK = 3;
-const int SHORT_BREAK = 2;
-const int WORKING = 1;
 
-int   state;
-int   button3_state;
-int   button3_old_state;
-int   button4_state;
-int   button4_old_state;
-unsigned long old_millis;
-unsigned long restart = 0;
-int   minute;
-int   second;
-int   time_interval;
-int   read_round;
-int   afk_second;
-int   is_open = 1;
-
-int trigPin = 4;
-int echoPin = 5;
-
-
-int get_round = 0;
-int loss_focus_round = 0;
 
 void loop() {
-  button3_old_state = button3_state;
-  button3_state = digitalRead(3);
-  button4_old_state = button4_state;
-  button4_state = digitalRead(2);
+  /* update for state*/
+  stop_button_old_state = stop_button_state;
+  stop_button_state = digitalRead(stopButton);
+  restart_button_old_state = restart_button_state;
+  restart_button_state = digitalRead(restartButton);
 
-  if (button3_old_state == 1 && button3_state == 0)
+  // stop button
+  if (stop_button_old_state == 1 && stop_button_state == 0)
   {
     switch (state)
     {
@@ -211,17 +240,18 @@ void loop() {
   }
 
   // restart button
-  if (button4_old_state == 1 && button4_state == 0 && ((restart != 0 && millis() - restart >= 1000) || restart == 0))
+  if (restart_button_old_state == 1 && restart_button_state == 0 && ((restart != 0 && millis() - restart >= 1000) || restart == 0))
   {
     restart = millis();
+    /* toggle to not open close all the lcd*/
     if (is_open)
     {
       is_open = 0;
       init();
       lcd.clear();
-      digitalWrite(9, HIGH);
-      digitalWrite(10, HIGH);
-      digitalWrite(11, HIGH);
+      digitalWrite(BLUE, HIGH);
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(RED, HIGH);
       lcd.noBacklight();
     }
     else
@@ -231,6 +261,8 @@ void loop() {
       lcd.setCursor(15, 0);
       lcd.write(byte(5));
       lcd.backlight();
+
+      /* reset session like close and real open*/
       fb.getInt("Session", session);
       stored_round = String("session") + session;
       stored_session = stored_round + "/status";
@@ -264,8 +296,10 @@ void loop() {
     }
   }
   
+  // if time not stop
   if (state == 1)
   {
+    // count time when pass 1 second
     if (millis() - old_millis >= 1000)
     {
       second += 1;
@@ -275,6 +309,8 @@ void loop() {
         second = 0;
       }
       print_timer();
+
+      // if timer is over
       if (minute == time_interval)
       {
         state = 0;
@@ -295,6 +331,7 @@ void loop() {
         minute = 0;
         second = 0;
 
+        // set new time interval
         if (read_round % 8 == 0)
           time_interval = LONG_BREAK;
         else if (read_round % 2 == 1)
@@ -302,15 +339,18 @@ void loop() {
         else
           time_interval = SHORT_BREAK;
 
+        // print new time interval on LCD
         lcd.setCursor(0, 1);
         if (time_interval < 10)
           lcd.print(0);
         lcd.print(time_interval);
         lcd.print(":00");
-
-        
+        afk_second = 0;
+        return ;
       }
       old_millis = millis();
+
+      // Check that you're still sitting
       if (time_interval == WORKING)
       {
         digitalWrite(trigPin, LOW);
@@ -324,29 +364,34 @@ void loop() {
         Serial.print(afk_second);
         Serial.print(" ");
         Serial.println(duration);
+
+        // if you far from desk more than 60 cm
         if (duration > 60)
         {
           afk_second += 1;
+
+          // when you far for 5 second
           if (afk_second > 5 && afk_second % 2)
           {
-            digitalWrite(9, HIGH);
-            digitalWrite(10, HIGH);
-            digitalWrite(11, LOW);
+            digitalWrite(BLUE, HIGH);
+            digitalWrite(GREEN, HIGH);
+            digitalWrite(RED, LOW);
           }
           else
           {
-            digitalWrite(9, HIGH);
-            digitalWrite(10, HIGH);
-            digitalWrite(11, HIGH);
+            digitalWrite(BLUE, HIGH);
+            digitalWrite(GREEN, HIGH);
+            digitalWrite(RED, HIGH);
           }
         }
         else
         {
           afk_second = 0;
-          digitalWrite(9, HIGH);
-          digitalWrite(10, HIGH);
-          digitalWrite(11, HIGH);
+          digitalWrite(BLUE, HIGH);
+          digitalWrite(GREEN, HIGH);
+          digitalWrite(RED, HIGH);
         }
+        // if you leave for 10 second stop the timer
         if (afk_second >= 10)
         {
           state = 0;
@@ -364,19 +409,19 @@ void loop() {
   }
 }
 
+// set state and many variable
+
 void  init()
 {
-  
-
   lcd.clear();
   state = 0;
-  button3_state = 1;
-  button3_old_state = 1;
-  button4_state = 1;
-  button4_old_state = 1;
-  digitalWrite(9, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(11, LOW);
+  stop_button_state = 1;
+  stop_button_old_state = 1;
+  restart_button_state = 1;
+  restart_button_old_state = 1;
+  digitalWrite(BLUE, HIGH);
+  digitalWrite(GREEN, LOW);
+  digitalWrite(RED, LOW);
   minute = 0;
   second = 0;
   time_interval = WORKING;
@@ -391,6 +436,7 @@ void  init()
   get_round = 0;
 }
 
+/* for changing state*/
 void  state_change()
 {
   lcd.setCursor(0, 0);
@@ -401,9 +447,9 @@ void  state_change()
         lcd.print("Stopped    ");
         lcd.setCursor(15, 0);
           lcd.write(byte(2));
-        digitalWrite(9, HIGH);
-        digitalWrite(10, HIGH);
-        digitalWrite(11, LOW);
+        digitalWrite(BLUE, HIGH);
+        digitalWrite(GREEN, HIGH);
+        digitalWrite(RED, LOW);
         afk_second = 0;
         break;
       case (1): // run timer
@@ -412,27 +458,27 @@ void  state_change()
           lcd.print("Long Break ");
           lcd.setCursor(15, 0);
           lcd.write(byte(4));
-          digitalWrite(9, LOW);
-          digitalWrite(10, HIGH);
-          digitalWrite(11, HIGH);
+          digitalWrite(BLUE, LOW);
+          digitalWrite(GREEN, HIGH);
+          digitalWrite(RED, HIGH);
         } 
         else if (read_round % 2) 
         {
           lcd.print("Working    ");
           lcd.setCursor(15, 0);
           lcd.write(byte(1));
-          digitalWrite(9, HIGH);
-          digitalWrite(10, HIGH);
-          digitalWrite(11, HIGH);
+          digitalWrite(BLUE, HIGH);
+          digitalWrite(GREEN, HIGH);
+          digitalWrite(RED, HIGH);
         }
         else
         {
           lcd.print("Short Break");
           lcd.setCursor(15, 0);
           lcd.write(byte(3));
-          digitalWrite(9, HIGH);
-          digitalWrite(10, LOW);
-          digitalWrite(11, HIGH);
+          digitalWrite(BLUE, HIGH);
+          digitalWrite(GREEN, LOW);
+          digitalWrite(RED, HIGH);
         }
         old_millis = millis();
         break;
