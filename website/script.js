b04f5d6c3141f6046ec7519ee2b2497503c16bea
GEOFFCHARGE <office.wanlachai@gmail.com>
2025-11-02 16:34:33 +0700

    Add contributors image cycle

--- website/script.js
+++ website/script.js
@@ -45,3 +45,25 @@ $("#radioDefault3").click(function()
     localStorage.setItem("theme", "auto");
     applyTheme("auto");
 });
+
+// MARK: cycle image
+let imagePupe = ["../image/member/pupe1.png", "../image/member/pupe2.png"];
+let imageOffice = ["../image/member/office1.png", "../image/member/office2.png"];
+let imageArse = ["../image/member/arse1.png", "../image/member/arse2.png"];
+let imageKia = ["../image/member/kia1.png", "../image/member/kia2.png"];
+let index = 0;
+const intervalTime = 3000;
+
+function cycleImage()
+{
+    index = (index + 1) % 2;
+    $("#pupe").attr("src", imagePupe[index]);
+    $("#office").attr("src", imageOffice[index]);
+    $("#arse").attr("src", imageArse[index]);
+    $("#kia").attr("src", imageKia[index]);
+}
+
+$(document).ready(function()
+{
+    setInterval(cycleImage, intervalTime);
+});
